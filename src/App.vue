517720e1ceedbe0ee119286425dfa2ddd7fc56a0
kingcedru <byishimocedrick@gmail.com>
2026-08-25 22:40:53 +0200

    feat: add router links

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,10 @@
     <nav>
       <ul>
         <li class="nav-item">
-          <img class="logo" src="./assets/build-a-bot-logo.png"/>
-          Build a bot
+          <router-link class="nav-link" :to="{name: 'Home'}">
+            <img class="logo" src="./assets/build-a-bot-logo.png"/>
+            Build a bot
+          </router-link>
         </li>
       </ul>
     </nav>
@@ -56,5 +58,9 @@ ul {
 .logo {
   vertical-align: middle;
   height: 30px;
+}
+.nav-link {
+  text-decoration: none;
+  color: inherit;
 }
 </style>
