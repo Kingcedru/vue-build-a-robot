b13d896ac72dcf4c96ac2483f75aaf2d422fd022
kingcedru <byishimocedrick@gmail.com>
2026-08-25 22:32:43 +0200

    feat: setting up routes

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,13 @@
     </nav>
   </header>
   <main>
-    <RobotBuilder />
+    <router-view />
   </main>
 </template>
 
 <script>
-import RobotBuilder from './build/RobotBuilder.vue';
-
 export default {
   name: 'App',
-  components: {
-    RobotBuilder,
-  },
 };
 </script>
 
