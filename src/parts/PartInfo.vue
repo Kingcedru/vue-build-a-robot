fd4ffff1368b6de3a49464952279d541d428085c
kingcedru <byishimocedrick@gmail.com>
2026-08-26 09:58:19 +0200

    feat: passing params as props

--- src/parts/PartInfo.vue
+++ src/parts/PartInfo.vue
@@ -12,9 +12,13 @@ import parts from '../data/parts';
 
 export default {
     name: 'PartInfo',
+    props: {
+        partType: {type: String, required: true},
+        id: {type: String, validator(value) {return Number.isInteger(+value);}, required: true}
+    },
     computed: {
         part(){
-            const {partType, id} = this.$route.params;
+            const {partType, id} = this
             return parts[partType].find(part => part.id === +id);
         }
     },
