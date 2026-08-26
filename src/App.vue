bc213fb5e9d9a09f7a31541fb5674be72f15546c
kingcedru <byishimocedrick@gmail.com>
2026-08-26 10:35:33 +0200

    feat: name views

--- src/App.vue
+++ src/App.vue
@@ -16,9 +16,14 @@
       </ul>
     </nav>
   </header>
-  <main>
-    <router-view />
-  </main>
+  <div class="container">
+    <aside>
+      <router-view name="sidebar" />
+    </aside>
+    <main>
+      <router-view />
+    </main>
+  </div>
 </template>
 
 <script>
@@ -39,15 +44,14 @@ body {
 
 <style scoped>
 main {
-  margin: 0 auto;
   padding: 30px;
   background-color: white;
-  width: 1024px;
+  width: 964px;
   min-height: 300px;
 }
 header {
   background-color: #999;
-  width: 1084px;
+  width: 1184px;
   margin: 0 auto;
 }
 ul {
@@ -70,5 +74,16 @@ ul {
 }
 .router-link-active {
   color: #e4e4e4;
+}
+.container {
+  display: flex;
+  margin:10px auto 0 auto;
+  justify-content: center;
+}
+aside {
+  padding: 30px;
+  width: 100px;
+  background-color: #aaa;
+  min-height: 300px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,8 @@ import RobotHeads from '../parts/RobotHeads.vue';
 import RobotArms from '../parts/RobotArms.vue';
 import RobotTorsos from '../parts/RobotTorsos.vue';
 import RobotBases from '../parts/RobotBases.vue';
+import SidebarStandard from '../sidebars/SidebarStandard.vue';
+import SidebarBuild from '../sidebars/SidebarBuild.vue';
 
 export default createRouter({
     history: createWebHashHistory(),
@@ -14,11 +16,17 @@ export default createRouter({
         {
             path: '/',
             name: 'Home',
-            component: HomePage,
+            components: {
+                default: HomePage,
+                sidebar: SidebarStandard
+            }
         }, {
             path: '/build',
             name: 'Build',
-            component: RobotBuilder,
+            components: {
+                default: RobotBuilder,
+                sidebar: SidebarBuild
+            }
         }, {
             path: '/parts/browse',
             name: 'BrowseParts',
